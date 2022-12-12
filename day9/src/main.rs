a0c06c264c222b47bef404bc5bb9c78c78c70e9b
skimdz86 <skimdz86@gmail.com>
2022-12-12 19:05:26 +0100

    setup day9

---
+++ day9/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+
+    // modello lista posizioni passate con matrice
+    // per capire quando si muove la tail, basta che gli indici di H siano >=T+2 i <=T-2 in ogni direzione (ne basta 1 >=2)
+}
